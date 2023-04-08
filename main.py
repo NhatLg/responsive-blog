40bb1a97e302280df4aff40423b2cd899d2c618e
Nhat <50206162+NhatLg@users.noreply.github.com>
2023-04-08 21:47:49 +0200

    finish configuring contact and about page

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def get_all_posts():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
