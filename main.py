493fe94d1c32e3ef368c2afacb96ac6b8d89bc9d
Nhat <50206162+NhatLg@users.noreply.github.com>
2023-04-11 20:13:05 +0200

    implement contact form

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import requests
 
 app = Flask(__name__)
 
+blog_contents = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 @app.route("/")
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=blog_contents)
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
